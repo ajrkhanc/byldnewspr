5fd2e48baafeeca4c8bc90da9a50f5697957bf61
Ajr khan <git config --global user.email you@example.com>
2022-11-22 12:49:04 +0530

    first commit

---
+++ app/models/post.js
@@ -0,0 +1,58 @@
+var mongoose = require('mongoose');
+var uniqueValidator = require('mongoose-unique-validator');
+
+var Schema = mongoose.Schema;
+
+var postSchema = new Schema({
+    Title: {
+        type: String,
+        required: true
+    },
+    posturl:{
+        type: String,
+        required: true,
+        unique:true
+    },
+
+    PostNewURL: {
+        type: String,
+        default : '#'
+    },
+
+    Author:{
+        type: String,
+        required: true
+    },
+    AuthorFullname:{
+        type: String,
+        required: true
+    },
+    
+    category: [String],
+    ImageURL : {
+        type: String, 
+        default : '#'
+    },
+    ImageAlt : {
+        type: String,
+        default : '#'
+    },
+   
+    visitors: {
+        type: Number,
+        default : 0
+    },
+    PublishDate: Date,
+    ModifiedDate : {
+        type: String
+    },
+    isPublished: {
+        type: String,
+        enum: ['0', '1', '2'],
+        default: '1'
+    }
+});
+
+// postSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model('Post',postSchema);
